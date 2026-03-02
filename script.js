fd4229405c238e1765b51c6b7344effdc3db2bf1
BilionIevgeniy <bilionjenia1111@gmail.com>
2026-03-02 13:41:52 +0100

    added fixions

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 const images = [
-  "alaska-810433_1280.jpg",
-  "moorente-8783210_1280.jpg",
-  "anime-8788959_1280.jpg",
-  "sea-2563389_1280.jpg",
-  "atmosphere-8752835_1280.png",
-  "snow-bunting-6781122_1280.jpg",
-  "blue-tit-8521052_1280.jpg",
-  "snow-leopard-cubs-8039138_1280.jpg",
-  "hurricane-92968_1280.jpg",
-  "travel-8785493_1280.jpg",
-  "lake-2896379_1280.jpg",
-  "winter-1675197_1280.jpg",
+  {
+    src: "alaska.jpg",
+    titel: "Alaska",
+  },
+  {
+    src: "moorente.jpg",
+    titel: "Moorente",
+  },
+  {
+    src: "anime.jpg",
+    titel: "Anime",
+  },
+  {
+    src: "sea.jpg",
+    titel: "Sea",
+  },
+  {
+    src: "atmosphere.png",
+    titel: "Atmosphere",
+  },
+  {
+    src: "snow-bunting.jpg",
+    titel: "Anow Bunting",
+  },
+  {
+    src: "blue-tit.jpg",
+    titel: "Blue Tit",
+  },
+  {
+    src: "snow-leopard-cubs.jpg",
+    titel: "Snow Leopard Cubs",
+  },
+  {
+    src: "hurricane.jpg",
+    titel: "Hurricane",
+  },
+  {
+    src: "travel.jpg",
+    titel: "Travel",
+  },
+  {
+    src: "lake.jpg",
+    titel: "Lake",
+  },
+  {
+    src: "winter.jpg",
+    titel: "Winter",
+  },
 ];
 let newModalImgIdx = 0;
 
@@ -18,18 +54,8 @@ function render() {
   const container = document.querySelector(".main_content");
   if (container) {
     const imgContent = images
-      .map((src, idx) => {
-        return /*html*/ `
-          <div 
-            onclick="openModal('${src}', ${idx})"
-            onkeydown="openModalOnEnter(event, '${src}', ${idx})"
-            class="gallery-item" 
-            tabindex="0" 
-            role="button"
-            aria-label="Open photo">
-              <img src="./assets/img/main_grid/${src}"/>
-          </div>
-        `;
+      .map(({ src, titel }, idx) => {
+        return generateGaleryImgTemplate(idx, src, titel);
       })
       .join("");
     container.innerHTML = imgContent;
@@ -39,14 +65,14 @@ function render() {
 function openModalOnEnter(event, src, idx) {
   if (event.key === "Enter" || event.key === " ") {
     event.preventDefault(); // чтобы страница не скроллилась от пробела
-    openModal(src, idx);
+    openModal(idx);
   }
 }
 
-function openModal(src, idx) {
+function openModal(idx) {
   document.body.innerHTML += `<div class="modal" onclick="closeModal()"></div>`;
   document.addEventListener("keydown", handleKeyboard);
-  setModalContent("modal", idx, src);
+  setModalContent("modal", idx);
   newModalImgIdx = idx;
 }
 
@@ -66,34 +92,14 @@ function switchModalImg(idx, isPrew) {
   } else {
     newModalImgIdx = isPrew ? idx - 1 : idx + 1;
   }
-  setModalContent("modal", newModalImgIdx, images[newModalImgIdx]);
+  setModalContent("modal", newModalImgIdx);
 }
 
-function setModalContent(className, idx, src) {
+function setModalContent(className, idx) {
   const modal = document.querySelector("." + className);
   if (modal) {
-    modal.innerHTML = /*html*/ `
-      <div class="modal_content" onclick="event.stopPropagation()">
-        <header class="modal_header">
-          <p>${src}</p>
-          <button onclick="closeModal()" class="close_btn">
-            <img class="close_img" src="./assets/img/icons/close.png" alt="close modal"/>
-          </button>
-        </header>
-        <img class="modal_main_img" src="./assets/img/main_grid/${src}" alt="modal img"/>
-        <div class="modal_footer">
-          <button onclick="switchModalImg(${idx}, ${true})" class="btn_left">
-            <img src="./assets/img/icons/Union.png" alt="left arrow"/>
-          </button>
-          <p class="modal_slides_info">
-            ${idx + 1} / ${images.length}
-          </p>
-          <button onclick="switchModalImg(${idx})" class="btn_right">
-            <img src="./assets/img/icons/Union.png" alt="right arrow"/>
-          </button>
-        </div>
-      </div>
-    `;
+    const { src, titel } = images[idx];
+    modal.innerHTML = generateModalContentTemplate(idx, titel, src);
   }
 }
 
@@ -105,4 +111,43 @@ function handleKeyboard(event) {
   } else if (event.key === "ArrowLeft") {
     switchModalImg(newModalImgIdx, true);
   }
+}
+
+function generateGaleryImgTemplate(idx, src, titel) {
+  return /*html*/ `
+          <div 
+            onclick="openModal(${idx})"
+            onkeydown="openModalOnEnter(event, '${src}', ${idx})"
+            class="gallery-item" 
+            tabindex="0" 
+            role="button"
+            aria-label="Open photo">
+              <img src="./assets/img/main_grid/${src}" alt='${titel} image'/>
+          </div>
+        `;
+}
+
+function generateModalContentTemplate(idx, titel, src) {
+  return /*html*/ `
+      <div class="modal_content" onclick="event.stopPropagation()">
+        <header class="modal_header">
+          <p>${titel}</p>
+          <button onclick="closeModal()" class="close_btn" aria-label="Close modal">
+            <img class="close_img" src="./assets/img/icons/close.png" alt="close modal"/>
+          </button>
+        </header>
+        <img class="modal_main_img" src="./assets/img/main_grid/${src}" alt="${titel} image"/>
+        <div class="modal_footer">
+          <button onclick="switchModalImg(${idx}, ${true})" class="btn_left" aria-label="Open prev photo">
+            <img src="./assets/img/icons/Union.png" alt="Open prev photo"/>
+          </button>
+          <p class="modal_slides_info">
+            ${idx + 1} / ${images.length}
+          </p>
+          <button onclick="switchModalImg(${idx})" class="btn_right" aria-label="Open next photo">
+            <img src="./assets/img/icons/Union.png" alt="Open next photo"/>
+          </button>
+        </div>
+      </div>
+    `;
 }
